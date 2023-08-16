9fddb2c250dabafb71088153a716adbdc8d95fd3
Valeria Castañeda <valeriacastaneda1120@gmail.com>
2023-08-16 08:28:36 -0500

    Primer commit del dashboard basico

---
+++ src/app/sales-month/sales-month.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SalesMonthComponent } from './sales-month.component';
+
+describe('SalesMonthComponent', () => {
+  let component: SalesMonthComponent;
+  let fixture: ComponentFixture<SalesMonthComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SalesMonthComponent]
+    });
+    fixture = TestBed.createComponent(SalesMonthComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
